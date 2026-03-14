0d88197460483b0d100fd05ffacddaa9b47c30fd
Juliya Boiko <boikoj@ou.org>
2026-03-14 22:24:31 +0200

    comment

--- app/plugins/client.ts
+++ app/plugins/client.ts
@@ -1,19 +1,19 @@
-import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
-import type { AppRouter } from '../../server/appRouter';
+// import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
+// import type { AppRouter } from '../../server/appRouter';
 
 export default defineNuxtPlugin(() => {
   // Create a tRPC client for the app
-  const client = createTRPCNuxtClient<AppRouter>({
-    links: [
-      httpBatchLink({
-        url: '/api/trpc', // tRPC endpoint
-      }),
-    ],
-  })
+  // const client = createTRPCNuxtClient<AppRouter>({
+  //   links: [
+  //     httpBatchLink({
+  //       url: '/api/trpc', // tRPC endpoint
+  //     }),
+  //   ],
+  // })
 
-  return {
-    provide: {
-      client, // make client available globally as $client
-    },
-  }
+  // return {
+  //   provide: {
+  //     client, // make client available globally as $client
+  //   },
+  // }
 })
