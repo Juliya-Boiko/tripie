1ed13d4b1e0ff88cff8fd64eb12e2b9be128d9a4
Juliya Boiko <boikoj@ou.org>
2026-03-14 22:00:40 +0200

    server

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    App nuxt !!!!!
+    App nuxt  v2 !!!!!
   </div>
 </template>
+
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+onMounted(async () => {
+  try {
+    const test = await $client.user.test.query()
+    console.log('HEllo', { test })
+  } catch (err) {
+    console.error(err)
+  }
+})
+</script>
---
+++ server/api/trpc/[trpc].ts
@@ -0,0 +1,17 @@
+import { createNuxtApiHandler } from 'trpc-nuxt/server'
+import { appRouter } from '~~/server/appRouter'
+import { createContext as trpcCreateContext } from '../../context'
+import type { H3Event } from 'h3'
+
+/**
+ * Nuxt API handler for tRPC
+ * - Uses `appRouter` to handle all procedures
+ * - Creates context for each request (extracts userId from auth token)
+ */
+export default createNuxtApiHandler({
+  router: appRouter,
+  createContext: async (event: H3Event) => {
+    // `event` is the H3Event object for the request
+    return trpcCreateContext({ event })
+  },
+})
